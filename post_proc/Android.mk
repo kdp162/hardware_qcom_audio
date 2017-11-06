0f74c34e507fc5acbde666e2b620d2dc9934f39a
Chih-Hung Hsieh <chh@google.com>
2017-11-06 11:39:27 -0800

    Use -Werror in hardware/qcom/audio
    * Suppress existing warnings.
    
    Bug: 66996870
    Test: build with WITH_TIDY=1
    Change-Id: I389b943471ccdcfc9b7db4411fa3973d77303a27

--- post_proc/Android.mk
+++ post_proc/Android.mk
@@ -6,7 +6,6 @@ qcom_post_proc_common_cflags := \
     -Wall -Werror \
     -Wno-unused-function \
     -Wno-unused-variable \
-    -Wno-sign-compare -Wno-format
 
 include $(CLEAR_VARS)
 
@@ -33,7 +32,7 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_AUDIOSPHERE)),true)
     LOCAL_SRC_FILES += asphere.c
 endif
 
-LOCAL_CFLAGS+= -O2 -fvisibility=hidden
+LOCAL_CFLAGS += $(qcom_post_proc_common_cflags)
 
 ifneq ($(strip $(AUDIO_FEATURE_DISABLED_DTS_EAGLE)),true)
     LOCAL_CFLAGS += -DDTS_EAGLE
@@ -76,7 +75,7 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_CFLAGS += -O2 -fvisibility=hidden
+LOCAL_CFLAGS += $(qcom_post_proc_common_cflags)
 
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_DTS_EAGLE)), true)
 LOCAL_CFLAGS += -DHW_ACC_HPX
